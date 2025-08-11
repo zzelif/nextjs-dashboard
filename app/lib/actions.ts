96dc114c2446b825106660062aa1622ba08eaf96
dan <dan.lettac@gmail.com>
2025-08-11 17:49:44 +0800

    add: authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,6 +6,9 @@ import { redirect } from "next/navigation";
 import postgres from "postgres";
 import { error } from "console";
 
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
+
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 const FormSchema = z.object({
@@ -108,4 +111,23 @@ export async function deleteInvoice(id: string) {
   await sql`DELETE FROM invoices WHERE id = ${id}`;
 
   revalidatePath("/dashboard/invoices");
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong.";
+      }
+    }
+    throw error;
+  }
 }
---
+++ app/login/page.tsx
@@ -0,0 +1,20 @@
+import AcmeLogo from "../ui/acme-logo";
+import LoginForm from "../ui/login-form";
+import { Suspense } from "react";
+
+export default function LoginPage() {
+  return (
+    <main className="flex items-center justify-center md:h-screen">
+      <div className="relative mx-auto flex w-full flex-col p-4 md:-mt-32 space-y-2.5 max-w-[400px]">
+        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
+          <div className="w-32 text-white md:w-36">
+            <AcmeLogo />
+          </div>
+        </div>
+        <Suspense>
+          <LoginForm />
+        </Suspense>
+      </div>
+    </main>
+  );
+}
